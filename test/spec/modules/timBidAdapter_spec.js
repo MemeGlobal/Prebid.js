35f9ac38555af257f685d74704c7fd9954e7de70
boris <boris@thetimmedia.com>
2019-01-24 10:58:14 +0200

    fix lint

--- test/spec/modules/timBidAdapter_spec.js
+++ test/spec/modules/timBidAdapter_spec.js
@@ -105,16 +105,16 @@ describe('timAdapterTests', function () {
       'body': {'id': '48640869bd9db94',
         'seatbid': [
           {'bid': [{'id': 'sti0a8b9aca-6080-49f6-bd63-ba54439d2359',
-              'impid': '4746fcaa11197f3',
-              'price': 3,
-              'nurl': 'https://eu.stinger-bidder.tech/api/v1/services/win?seller=prebid&price=${AUCTION_PRICE}&bidcur=USD&bidid=sti0a8b9aca-6080-49f6-bd63-ba54439d2359&strict=true',
-              'adm': '<script type=\"text/javascript\" id=\"main_script\" src=\"https://growfoodsmart.com/sas/player/directPlayer.php?iid=sti0a8b9aca-6080-49f6-bd63-ba54439d2359&sPlatform=hb&activity=hb&plc=placementCodeTest&width=300&height=250&sCampaignID=50106601&sSeller=testpublisherid&creativeID=700&cb=79f1826c-7f5e-44a2-aa59-314ed8b7e470&sCost=3.000&sBid=3.000&sDomain=mediamart.tv&ab=&udevice=desktop&sDeviceType=desktop&uid=1bf1cc21-26db-4c45-8b72-f3e6b1a3008c&ubrowser=chrome&m=MTI4MzM5ODUyODQ1MQ%3D%3D&uOS=windows_10&ot=SCANNER\"></script>',
-              'adomain': [''],
-              'cid': '1',
-              'crid': '700',
-              'w': 300,
-              'h': 250
-            }]}],
+            'impid': '4746fcaa11197f3',
+            'price': 3,
+            'nurl': 'https://eu.stinger-bidder.tech/api/v1/services/win?seller=prebid&price=${AUCTION_PRICE}&bidcur=USD&bidid=sti0a8b9aca-6080-49f6-bd63-ba54439d2359&strict=true',
+            'adm': '<script type=\"text/javascript\" id=\"main_script\" src=\"https://growfoodsmart.com/sas/player/directPlayer.php?iid=sti0a8b9aca-6080-49f6-bd63-ba54439d2359&sPlatform=hb&activity=hb&plc=placementCodeTest&width=300&height=250&sCampaignID=50106601&sSeller=testpublisherid&creativeID=700&cb=79f1826c-7f5e-44a2-aa59-314ed8b7e470&sCost=3.000&sBid=3.000&sDomain=mediamart.tv&ab=&udevice=desktop&sDeviceType=desktop&uid=1bf1cc21-26db-4c45-8b72-f3e6b1a3008c&ubrowser=chrome&m=MTI4MzM5ODUyODQ1MQ%3D%3D&uOS=windows_10&ot=SCANNER\"></script>',
+            'adomain': [''],
+            'cid': '1',
+            'crid': '700',
+            'w': 300,
+            'h': 250
+          }]}],
         'bidid': 'sti0a8b9aca-6080-49f6-bd63-ba54439d2359',
         'cur': 'USD'
       },
